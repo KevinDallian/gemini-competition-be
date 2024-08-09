4d3611998610ded4c7823b9fd87b2f5bad271755
Kevin Dallian <kevindallian@gmail.com>
2024-08-09 21:07:13 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
